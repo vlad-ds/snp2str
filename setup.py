4809ff5960a7f2a73e9872bd2dcaff142f272cc0
Vlad Gheorghe <contact@vlad-gheorghe.com>
2022-11-12 13:18:23 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-# TODO add requirements
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='snp2str',
       version='0.1',
       description='TODO',
-      url='http://github.com/TODO',
+      url='http://github.com/TODO', # TODO add public git url
       author='Vlad Gheorghe',
       author_email='vlad.datapro@gmail.com',
       license='TODO',
       packages=['snp2str'],
       scripts=['bin/snp2str'],
+      install_requires=required,
       include_package_data=True,
       zip_safe=False)
